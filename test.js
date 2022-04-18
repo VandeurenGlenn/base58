da83b0353ed3feac66404b020d07d60ec6a0704a
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-18 20:34:05 +0200

    Add isBase58 method

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const uint8Array = new TextEncoder().encode('hi')
 const base58Encoded = base58.encode(uint8Array)
 
 console.log(base58Encoded)
-
+console.log(base58.isBase58(base58Encoded));
 const base58Decoded = base58.decode(base58Encoded)
 
 const string = new TextDecoder().decode(base58Decoded)
