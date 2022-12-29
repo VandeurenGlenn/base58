4c32fa59a3381780769f38f4512430c9264fc9ac
vandeurenglenn <vandeurenglenn@gmail.com>
2022-12-29 17:12:06 +0100

    Add base58Hex

--- src/base58.ts
+++ src/base58.ts
@@ -1,10 +1,16 @@
 import baseX from '@vandeurenglenn/base-x'
 
 const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+const ALPHABET_HEX = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuv'
+
 const base58 = baseX(ALPHABET)
+const base58Hex = baseX(ALPHABET_HEX)
 
 export const encode = base58.encode
 export const decode = base58.decode
+export const encodeHex = base58Hex.encode
+export const decodeHex = base58Hex.decode
+
 export const isBase58 = (string: base58String): boolean => {
   try {
     decode(string)
@@ -13,5 +19,4 @@ export const isBase58 = (string: base58String): boolean => {
     return false
   }
 }
-
-export default { encode, decode, isBase58 }
+export default { encode, decode, isBase58, encodeHex, decodeHex }
--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 import base58 from './dist/base58.js'
 
-const uint8Array = new TextEncoder().encode('hi')
+const uint8Array = new TextEncoder().encode('hello')
 
 const base58Encoded = base58.encode(uint8Array)
+const base58EncodedHex = base58.encodeHex(uint8Array)
 
 console.log(base58Encoded)
+console.log(base58EncodedHex)
 console.log(base58.isBase58(base58Encoded));
+console.log(base58.isBase58(base58EncodedHex));
 const base58Decoded = base58.decode(base58Encoded)
+const base58DecodedHex = base58.decodeHex(base58EncodedHex)
+
 
 const string = new TextDecoder().decode(base58Decoded)
+const string2 = new TextDecoder().decode(base58DecodedHex)
 
-console.log(string)
+console.log(string === string2)
